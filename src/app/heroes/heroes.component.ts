4f55e396509b802d5a02ba0ea8c4f30adf924319
James Wang <jwsb0702@gmail.com>
2018-09-09 16:58:50 -0400

    Completed Angular beginners guide, step 4: 'Displaying a list'

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
+
 export class HeroesComponent implements OnInit {
-	hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+	
+  selectedHero: Hero;
   
   constructor() {
 
@@ -18,6 +21,10 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit() {
   
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
